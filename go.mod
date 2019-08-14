78f646dcbdaae739b850eccb0c435b0c452ed1f0
Maya Li <maya@calm.com>
2019-08-13 22:57:21 -0700

    add statsd log provider

--- go.mod
+++ go.mod
@@ -4,11 +4,14 @@ go 1.12
 
 require (
 	github.com/ansel1/merry v1.3.2
+	github.com/etsy/statsd v0.8.2
 	github.com/go-errors/errors v1.0.1
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2
-	github.com/myhelix/rollbar v0.0.0-20160920221733-41394a789716
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/myhelix/rollbar v0.4.2
 	github.com/newrelic/go-agent v1.11.0
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
 	github.com/sirupsen/logrus v1.4.0
-	golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c
-	golang.org/x/sys v0.0.0-20190311152110-c8c8c57fd1e1
+	golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c // indirect
+	golang.org/x/sys v0.0.0-20190311152110-c8c8c57fd1e1 // indirect
 )
---
+++ providers/statsd/statsd.go
@@ -0,0 +1,38 @@
+package statsd
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/calm/contextlogger/providers"
+	"github.com/calm/contextlogger/providers/chaining"
+	etsystatsd "github.com/etsy/statsd/examples/go"
+)
+
+type provider struct {
+	sharedStatsdClient *etsystatsd.StatsdClient // This has to be passed in; we can't import package config
+	providers.LogProvider
+}
+
+func LogProvider(nextProvider providers.LogProvider,  client *etsystatsd.StatsdClient) (providers.LogProvider, error) {
+	if client == nil {
+		return nil, errors.New("newRelicApp is required")
+	}
+	return provider{client, chaining.LogProvider(nextProvider)}, nil
+}
+
+func (p provider) Record(ctx context.Context, metrics map[string]interface{}) {
+	statsToSend := make(map[string]string)
+	for k, v := range metrics {
+		statsToSend[k] = fmt.Sprintf("%d|g", v)
+	}
+
+	p.sharedStatsdClient.Send(statsToSend, 1)
+	p.LogProvider.Record(ctx, metrics)
+}
+
+func (p provider) RecordEvent(ctx context.Context, eventName string, metrics map[string]interface{}) {
+	p.sharedStatsdClient.Increment(eventName)
+	p.LogProvider.RecordEvent(ctx, eventName, metrics)
+}
+
