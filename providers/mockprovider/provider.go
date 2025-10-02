36c722ccac1b94bddc0825761aa81e34028d5300
Oliver Tucher <oliver.tucher@helix.com>
2025-10-02 11:54:58 -0700

    privatize structu

--- providers/mockprovider/provider.go
+++ providers/mockprovider/provider.go
@@ -8,46 +8,46 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-func LogProvider(nextProvider providers.LogProvider) *MockProvider {
-	return &MockProvider{LogProvider: chaining.LogProvider(nextProvider)}
+func LogProvider(nextProvider providers.LogProvider) *provider {
+	return &provider{LogProvider: chaining.LogProvider(nextProvider)}
 }
 
-type MockProvider struct {
+type provider struct {
 	mock.Mock
 	providers.LogProvider
 }
 
-func (p *MockProvider) Error(ctx context.Context, report bool, args ...interface{}) {
+func (p *provider) Error(ctx context.Context, report bool, args ...interface{}) {
 	p.Called(ctx, report, args)
 	p.LogProvider.Error(ctx, report, args...)
 }
 
-func (p *MockProvider) Warn(ctx context.Context, report bool, args ...interface{}) {
+func (p *provider) Warn(ctx context.Context, report bool, args ...interface{}) {
 	p.Called(ctx, report, args)
 	p.LogProvider.Warn(ctx, report, args...)
 }
 
-func (p *MockProvider) Info(ctx context.Context, report bool, args ...interface{}) {
+func (p *provider) Info(ctx context.Context, report bool, args ...interface{}) {
 	p.Called(ctx, report, args)
 	p.LogProvider.Info(ctx, report, args...)
 }
 
-func (p *MockProvider) Debug(ctx context.Context, report bool, args ...interface{}) {
+func (p *provider) Debug(ctx context.Context, report bool, args ...interface{}) {
 	p.Called(ctx, report, args)
 	p.LogProvider.Debug(ctx, report, args...)
 }
 
-func (p *MockProvider) Record(ctx context.Context, metrics map[string]interface{}) {
+func (p *provider) Record(ctx context.Context, metrics map[string]interface{}) {
 	p.Called(ctx, metrics)
 	p.LogProvider.Record(ctx, metrics)
 }
 
-func (p *MockProvider) RecordEvent(ctx context.Context, eventName string, metrics map[string]interface{}) {
+func (p *provider) RecordEvent(ctx context.Context, eventName string, metrics map[string]interface{}) {
 	p.Called(ctx, eventName, metrics)
 	p.LogProvider.RecordEvent(ctx, eventName, metrics)
 }
 
-func (p *MockProvider) Wait() {
+func (p *provider) Wait() {
 	p.Called()
 	p.LogProvider.Wait()
 }
